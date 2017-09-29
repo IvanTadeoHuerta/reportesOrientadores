c391b1469fd57f462170203d57881e53f75d4922
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-09-29 18:11:04 -0500

    Login correcto

--- src/components/login.vue
+++ src/components/login.vue
@@ -140,8 +140,7 @@ export default {
    ========================================================================== */
 
 html, body {
-    height: 100%;
-    width: 100%;
+   background-color: black
 }
 
 body {
