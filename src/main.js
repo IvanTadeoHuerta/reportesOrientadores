e4beb1b3188e19e36b7981cf23ec7fa974602c7e
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-09-29 16:58:34 -0500

    Se añadió configuración de rutas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import routes from './routes'
 import { store } from './store/store'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({ routes, mode: 'history'})
 
 new Vue({
   el: '#app',
   store,
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+/*
+ *Modulos que se integran a las rutas
+ */
+
+import Login from './components/login.vue'
+
+const routes = [
+    {path:'/', component:Login, name:'login'}
+]
+
+export default routes
