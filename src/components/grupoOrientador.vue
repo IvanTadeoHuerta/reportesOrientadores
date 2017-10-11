3010f561fac317052df4689c4df62dffd4b48c03
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-11 10:45:56 -0500

    Se actualizo la vista del componente grupoOrientador

--- src/components/grupoOrientador.vue
+++ src/components/grupoOrientador.vue
@@ -1,5 +1,150 @@
 <template>
-  <h1>Viendo información del grupo {{ $route.params.id }}</h1>
+   <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="table-responsive">
+          <table class="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nombre del alumno</th>
+                <th>Nombre del tutor</th>
+                <th>Reporte</th>
+                <th>Historial</th>
+                <th>Actualizar datos</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Alumnno registrado 1</td>
+                <td>Tutor registrado 1</td>
+                <td>
+                  <button class="btn btn-success">
+                    <span class="glyphicon glyphicon-plus"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-default">
+                    <span class="glyphicon glyphicon-th-list"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-danger">
+                    <span class="glyphicon glyphicon-trash"></span>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Alumnno registrado 2</td>
+                <td>Tutor registrado 2</td>
+                 <td>
+                  <button class="btn btn-success">
+                    <span class="glyphicon glyphicon-plus"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-default">
+                    <span class="glyphicon glyphicon-th-list"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-danger">
+                    <span class="glyphicon glyphicon-trash"></span>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Alumnno registrado 3</td>
+                <td>Tutor registrado 3</td>
+                 <td>
+                  <button class="btn btn-success">
+                    <span class="glyphicon glyphicon-plus"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-default">
+                    <span class="glyphicon glyphicon-th-list"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-danger">
+                    <span class="glyphicon glyphicon-trash"></span>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Alumnno registrado 4</td>
+                <td>Tutor registrado 4</td>
+                 <td>
+                  <button class="btn btn-success">
+                    <span class="glyphicon glyphicon-plus"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-default">
+                    <span class="glyphicon glyphicon-th-list"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-danger">
+                    <span class="glyphicon glyphicon-trash"></span>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>Alumnno registrado 5</td>
+                <td>Tutor registrado 5</td>
+                 <td>
+                  <button class="btn btn-success">
+                    <span class="glyphicon glyphicon-plus"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-default">
+                    <span class="glyphicon glyphicon-th-list"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-primary">
+                    <span class="glyphicon glyphicon-pencil"></span>
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-danger">
+                    <span class="glyphicon glyphicon-trash"></span>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
 </template>
 <script>
 export default {
