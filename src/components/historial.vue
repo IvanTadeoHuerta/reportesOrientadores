d0b1b546699776178af9669db45b2500455ffc46
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-11 11:23:29 -0500

    Se agregaron tabs en el componente de hijos

--- src/components/historial.vue
+++ src/components/historial.vue
@@ -1,5 +1,14 @@
 <template>
-    <h1>Historial del estudiante {{ $route.params.id}}</h1>
+  <div>
+    <ul class="nav nav-tabs">
+      <li class="active">
+        <a data-toggle="tab" href="#home">Reportes (1)</a>
+      </li>
+      <li>
+        <a data-toggle="tab" href="#menu1">Avisos (3)</a>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
