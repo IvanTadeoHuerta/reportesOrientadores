e3139602c193dfc4af4795c48881fe2d7a70d20b
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-12 14:21:07 -0500

    Correccion del modal para agregar grupos

--- src/components/grupos.vue
+++ src/components/grupos.vue
@@ -126,14 +126,11 @@
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
             <h4 class="modal-title">
               <b>{{titulo}}</b>
             </h4>
           </div>
-          <div class="modal-body table-responsive" style="overflow-x:auto; max-height: 400px; overflow-y:auto;">
+          <div class="modal-body" style="overflow-x:auto; max-height: 400px; overflow-y:auto;">
             <!-- Inicio del formulario -->
             <form role="form">
               <div class="form-group">
@@ -192,7 +189,7 @@
             <!-- Fin del formulario -->
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
             <button type="button" class="btn btn-primary">{{ textoBtn }}</button>
           </div>
         </div>
