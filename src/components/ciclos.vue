281af4c419310022d67f3af39937ef3395c15d83
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-12 14:24:12 -0500

    Correción de modal para agregar ciclo escolar

--- src/components/ciclos.vue
+++ src/components/ciclos.vue
@@ -118,9 +118,6 @@
                 <div class="modal-dialog modal-md" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
                             <h4 class="modal-title">
                                 <b>{{titulo}}</b>
                             </h4>
@@ -141,7 +138,7 @@
                             </div>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
                             <button type="button" class="btn btn-primary">{{ textoBtn }}</button>
                         </div>
                     </div>
