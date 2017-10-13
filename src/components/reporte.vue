6effdbafd3d9dd4f0ff82d7d0500fd2e56fba22b
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-10-13 16:26:11 -0500

    Formulario de reporte completo

--- src/components/reporte.vue
+++ src/components/reporte.vue
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <label for="fecha">Fecha: </label>
-                    <input type="text" class="form-control" name="fecha" value="" disabled>
+                    <input type="text" class="form-control" name="fecha" :value="fecha"  disabled>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <label for="alumno">Nombre del alumno: </label>
@@ -52,7 +52,12 @@
 </template>
 <script>
 export default {
-    name: 'reporte'
+    name: 'reporte',
+    data(){
+        return{
+            fecha: moment(new Date()).format('YYYY-MM-DD')
+        }
+    }
 }
 </script>
 
